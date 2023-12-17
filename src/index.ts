e38e813ae481bebcdeeded92a248a25b3f9c687d
vitaliystorchous <vitaliystorchous@gmail.com>
2023-12-17 05:34:40 +0100

    feat: possible values

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 export function requireEnvVar(envVarName: string, options?: {
   default?: string
-}) {
-  const value = process.env[envVarName]
-  if (value) {
-    return value
-  } else {
+  possibleValues?: string[]
+}): string {
+  let value = process.env[envVarName]
+  if (!value) {
     if (options?.default) {
-      return options.default
+      value = options.default
     } else {
       throw new Error(`environment variable is not defined: ${envVarName}`)
     }
   }
+
+  if (options?.possibleValues) {
+    const isOneOfPossible = options.possibleValues.includes(value)
+    if (!isOneOfPossible) {
+      let message: string
+      if (value === options.default) {
+        message = `default value for environment variable is unexpected`
+      } else {
+        message = `environment variable contains unexpected value`
+      }
+      message += `\nenvironment variable: ${envVarName}`
+        + `\nactual value: ${value}`
+        + `\npossible values: ${JSON.stringify(options.possibleValues)}`
+      throw new Error(message)
+    }
+  }
+
+  return value
 }
