6da16b00d214f6202d9ed516c09bc35f3535a9d6
vitaliystorchous <vitaliystorchous@gmail.com>
2023-12-17 06:02:52 +0100

    test: possible values

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,8 @@ export function requireEnvVar(envVarName: string, options?: {
   if (options?.possibleValues) {
     const isOneOfPossible = options.possibleValues.includes(value)
     if (!isOneOfPossible) {
-      let message: string
-      if (value === options.default) {
-        message = `default value for environment variable is unexpected`
-      } else {
-        message = `environment variable contains unexpected value`
-      }
-      message += `\nenvironment variable: ${envVarName}`
+      const message = `environment variable contains unexpected value`
+        + `\nenvironment variable: ${envVarName}`
         + `\nactual value: ${value}`
         + `\npossible values: ${JSON.stringify(options.possibleValues)}`
       throw new Error(message)
--- test/requireEnvVar.test.ts
+++ test/requireEnvVar.test.ts
@@ -3,13 +3,7 @@ import { faker } from "@faker-js/faker"
 
 describe('requireEnvVar', () => {
   test('env var is defined', () => {
-    const envVarName = faker.string.alpha({
-      length: {
-        min: 5,
-        max: 10,
-      },
-      casing: 'upper'
-    })
+    const envVarName = faker.string.alpha(5)
     const envVarValue = faker.person.fullName()
     process.env[envVarName] = envVarValue
     const value = requireEnvVar(envVarName)
@@ -23,13 +17,7 @@ describe('requireEnvVar', () => {
   })
 
   test('env var is defined with default value', () => {
-    const envVarName = faker.string.alpha({
-      length: {
-        min: 5,
-        max: 10,
-      },
-      casing: 'upper'
-    })
+    const envVarName = faker.string.alpha(5)
     const envVarValue = faker.person.fullName()
     const defaultEnvVarValue = faker.string.uuid()
     process.env[envVarName] = envVarValue
@@ -41,5 +29,71 @@ describe('requireEnvVar', () => {
     const defaultEnvVarValue = faker.string.uuid()
     const value = requireEnvVar('THIS_ENV_VAR_DOES_NOT_EXIST', { default: defaultEnvVarValue })
     expect(value).toBe(defaultEnvVarValue)
+  })
+
+  test('env var is defined that matches possible values', () => {
+    const envVarName = faker.string.alpha(5)
+    const possibleValues = [
+      faker.animal.cat(),
+      faker.animal.bear(),
+      faker.animal.bird()
+    ]
+    const envVarValue = faker.helpers.arrayElement(possibleValues)
+    process.env[envVarName] = envVarValue
+    const value = requireEnvVar(envVarName, { possibleValues: possibleValues })
+    expect(value).toBe(envVarValue)
+  })
+
+  test('env var is defined that does not match possible values', () => {
+    const envVarName = faker.string.alpha(5)
+    const possibleValues = [
+      faker.animal.cat(),
+      faker.animal.bear(),
+      faker.animal.bird()
+    ]
+    const envVarValue = faker.animal.cetacean()
+    process.env[envVarName] = envVarValue
+    const getEnvVarValue = () => requireEnvVar(envVarName, {
+      possibleValues
+    })
+    let expectedMessageError = `environment variable contains unexpected value`
+      + `\nenvironment variable: ${envVarName}`
+      + `\nactual value: ${envVarValue}`
+      + `\npossible values: ${JSON.stringify(possibleValues)}`
+    expect(getEnvVarValue).toThrow(new Error(expectedMessageError))
+  })
+
+  test('env var is not defined and its default value matches possible values', () => {
+    const envVarName = faker.string.alpha(5)
+    const possibleValues = [
+      faker.animal.cat(),
+      faker.animal.bear(),
+      faker.animal.bird()
+    ]
+    const defaultValue = faker.helpers.arrayElement(possibleValues)
+    const value = requireEnvVar(envVarName, {
+      possibleValues,
+      default: defaultValue
+    })
+    expect(value).toBe(defaultValue)
+  })
+
+  test('env var is not defined and its default value does not match possible values', () => {
+    const envVarName = faker.string.alpha(5)
+    const possibleValues = [
+      faker.animal.cat(),
+      faker.animal.bear(),
+      faker.animal.bird()
+    ]
+    const defaultValue = faker.animal.cetacean()
+    const getEnvVarValue = () => requireEnvVar(envVarName, {
+      possibleValues,
+      default: defaultValue
+    })
+    let expectedMessageError = `environment variable contains unexpected value`
+      + `\nenvironment variable: ${envVarName}`
+      + `\nactual value: ${defaultValue}`
+      + `\npossible values: ${JSON.stringify(possibleValues)}`
+    expect(getEnvVarValue).toThrow(new Error(expectedMessageError))
   })
 })
