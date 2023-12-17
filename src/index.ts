5ab9ab6b0ce2c11dcbb72d48e52b6d8a5cc617ca
vitaliystorchous <vitaliystorchous@gmail.com>
2023-12-17 04:52:44 +0100

    feat: default value

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-export function requireEnvVar(envVarName: string) {
+export function requireEnvVar(envVarName: string, options?: {
+  default?: string
+}) {
   const value = process.env[envVarName]
-  if (!value) {
-    throw new Error(`environment variable is not defined: ${envVarName}`)
-  } else {
+  if (value) {
     return value
+  } else {
+    if (options?.default) {
+      return options.default
+    } else {
+      throw new Error(`environment variable is not defined: ${envVarName}`)
+    }
   }
 }
